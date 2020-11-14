e0583f8541a0afb135495514038f8a4f1d00cfeb
ethan <o1098350250@gmail.com>
2020-11-14 23:12:22 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)    //ORDINAL로 설정 후 사용하게 되면 서비스 중에 STATUS 값이 추가가 되면
+                                    // DATA 순서가 꼬이기 때문에 STRING으로 사용할 것
+    private DeliveryStatus status;  //READY, COMP
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)   //상속관계 전략, 싱글 테이블 : 한 테이블에 전부 넣는 것
+@DiscriminatorColumn(name = "dtype")    //
+@Getter @Setter
+public abstract class Item {    //추가 클래스 - 구현체로 할거기 때문에
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+}
